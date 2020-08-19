221624290dcdc9bd83bf59a40691ec6adb6146c8
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-08-19 14:54:40 -0400

    Updated questions array and wrote init function

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const questions = [
     {
         type: 'input',
         name: 'username',
-        message: 'Enter your GitHub username'
+        message: 'Please enter your GitHub username:'
     },
     {
         type: 'input',
         name: 'email',
-        message: 'Enter the email adderess you use for your GitHub account'
+        message: 'Please enter your email adderess:'
     },
     {
         type: 'input',
@@ -21,82 +21,94 @@ const questions = [
     {
         type: 'input',
         name: 'description',
-        message: 'Write a description of your project.'
+        message: 'Write a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'url',
+        message: 'What is the link to your deployed project?'
     },
     {
         type: 'input',
         name: 'installation',
-        message: 'What are the instructions for installation?'
+        message: 'What command should be run to install dependencies?',
+        default: 'npm install'
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'What are the instructions for using your project?'
+        message: 'What are the instructions for using your project?',
     },
     {
         type: 'expand',
         name: 'license',
-        message: 'How are you licensing your repository?',
+        message: 'How are you licensing your project?',
         choices: [
-            {key:'afl', name: 'Academic Free License', value: 'afl-3.0'},
-            {key:'apa', name: 'Apache License', value: 'afl-3.0'},
-            {key:'art', name: 'Artistic License', value: 'artistic-2.0'},
-            {key:'bsl', name: 'Boost Software License', value: 'bsl-1.0'},
-            {key:'', name: '', value: ''},
-            {key:'', name: '', value: ''},
-            {key:'', name: '', value: ''},
-            {key:'cc', name: 'Creative Commons License Family', value: 'cc},
-            {key:'cca', name: 'Creative Commons Attribution 4.0', value: 'cc-by-4.0'},
-            {key:'', name: '', value: ''},
-            {key:'', name: '', value: ''},
-            {key:'wtf', name: 'Do What The F*ck You Want To Public License', value: 'wtfpl'},
-            {key:'ecl', name: 'Educational Community License v2.0', value: 'ecl-2.0'},
-            {key:'', name: '', value: ''}
-            {key:'eup', name: 'European Union Public License 1.1', value: 'eupl-1.1'},
-            {key:'gnu', name: 'GNU General Public License family', value: 'gpl'},
-            {key:'gnl', name: 'GNU Lesser General Public License family', value: 'lgpl'},
-            {key:'', name: '', value: ''},
-            {key:'', name: '', value: ''},
-            {key:'', name: '', value: ''},
-            {key:'', name: '', value: ''},
-            {key:'', name: '', value: ''},
-            {key:'isc', name: 'ISC', value: 'isc'},
-            {key:'lat', name: 'LaTeX Project Public License v1.3c', value: 'llpl-1.3c'},
-            {key:'mic', name: 'Microsoft Public License', value: 'ms-pl'},
-            {key:'mit', name: 'MIT', value: 'mit'},
-            {key:'moz', name: 'Mozilla Public License 2.0', value: 'mpl-2.0'},
-            {key:'osl', name: 'Open Software License 3.0', value: 'osl-3.0'},
-            {key:'pst', name: 'PostgreSQL License', value: 'postgresql'},
-            {key:'', name: '', value: ''},
-            {key:'', name: '', value: ''},
-            {key:'unl', name: 'The Unlicense', value: 'unlicense'},
-            {key:'zlb', name: 'zLib License', value: 'zlib'},
+            {key:'a', name: 'Apache License', value: 'apache-2.0'},
+            {key:'b', name: 'Boost Software License', value: 'bsl-1.0'},
+            {key:'c', name: 'BSD 2-Clause "Simplified" License', value: 'bsd-2-clause'},
+            {key:'d', name: 'BSD 3-Clause "New" or "Revised" License', value: 'bsd-3-clause'},
+            {key:'e', name: 'Creative Commons Zero v1.0 Universal', value: 'cc0-1.0'},
+            {key:'f', name: 'Creative Commons Attribution 4.0', value: 'cc-by-4.0'},
+            {key:'g', name: 'Eclipse Public License', value: 'epl-1.0'},
+            {key:'i', name: 'GNU Affero General Public License v3.0', value: 'agpl-3.0'},
+            {key:'j', name: 'GNU General Public License v2.0', value: 'gpl-2.0'},
+            {key:'k', name: 'GNU General Public License v3.0', value: 'gpl-3.0'},
+            {key:'l', name: 'GNU Lesser General Public License v3.0', value: 'lgpl-3.0'},
+            {key:'m', name: 'IBM Public License v1.0', value: 'ipl-1.0'},
+            {key:'n', name: 'MIT', value: 'mit'},
+            {key:'o', name: 'Mozilla Public License 2.0', value: 'mpl-2.0'},
+            {key:'p', name: 'Open Data Commons Attribute License (BY)', value: 'odc-by'},
+            {key:'q', name: 'Open Database License', value: 'odbl'},
+            {key:'r', name: 'Public Domain Dedication and License', value: 'pddl'},
+            {key:'s', name: 'The Artistic License 2.0', value: 'artisitic-2.0'},
+            {key:'t', name: 'The Unlicense', value: 'unlicense'},
+            {key:'u', name: 'zLib License', value: 'zlib'},
         ],
+        pageSize: 20
     },
     {
         type: 'input',
-        name: 'usage',
-        message: 'What are the instructions for using your project?'
+        name: 'contribution',
+        message: 'How can others contribute to your project?',
     },
     {
         type: 'input',
-        name: 'usage',
-        message: 'What are the instructions for using your project?'
+        name: 'testing',
+        message: 'What command should be used to run tests on your project?',
+        default: 'npm test'
     },
 ];
 
+// Use the inquirer module to get user input (answers to the questions).
 const promptUser = () => {
-    return inquirer.prompt(questions)
+    return inquirer.prompt(questions);
+}
+
+
+const generateREADME = (answers) => {
+    return ``
 }
 
 // from starter code:
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
-function init() {
+const init = async () => {
+    try {
+        const answers = await promptUser();
 
+        const readme = generateREADME(answers);
+
+        await writeToFile('README1.md', readme);
+
+        console.log('Successfully wrote to README.md');
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 // function call to initialize program
